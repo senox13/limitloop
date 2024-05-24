346bea65d08d8db104cfd2b9b94284c1d49fadbf
senox13 <senox13@gmail.com>
2024-05-24 09:24:40 -0700

    *  Fixed a math bug in calculating the FPS  *  `limitloop()` now returns the last value returned by the function passed to it  *  Added better docstrings to all functions, and the module itself  *  Renamed `demo.py` to `example.py` and changed the example to run at under the maximum FPS, on most machines, hopefully  *  Added a `READEME.TXT` that contains copies of all the docstrings in the module

--- example.py
+++ example.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-
 from limitloop import limitloop, endloop, getfps
+from math import sqrt
 
 i = 0
 def test_func():
     global i
     i += 1
     print("Iteration {}, FPS is {}".format(i, getfps()))
-    if i == 10:
-        print("Ending loop")
+    
+    for j in range(0, 1000000):
+        sqrt(j)
+    
+    if i == 10: #End loop when i reaches 10
         endloop()
+        print("Loop broken")
 
 if __name__ == "__main__":
     limitloop(test_func, 90)
-
--- limitloop.py
+++ limitloop.py
@@ -1,6 +1,13 @@
 from time import perf_counter, sleep
 import math
 
+"""
+limitloop.py is a simple module for running loops that need to be run at a
+consistant, limited speed
+"""
+
+__all__ = ['limitloop', 'endloop', 'getfps']
+
 #Globals
 DEFAULT_FPS_LIMIT = 30 #Equivalent to hz
 
@@ -9,23 +16,35 @@ _running = False
 _fps = 0
 
 def limitloop(function, fps_limit=DEFAULT_FPS_LIMIT, *args, **kwargs):
-    """Calls the supplied function with the args passed through *args and **kwargs in a loop, the speed of which is limited to fps_limit"""
+    """
+    Calls the supplied function as fast as possible, up to the number of times
+    per second specified in fps_limit. args and kwargs, if provided, are
+    passed to the called function
+    
+    :param function: The function or callable to call every frame
+    :param fps_limit: The maximum number of times per second to call the callable specified in function
+    :returns: The value returned by the last call to function, or None
+    """
     global _running
     global _fps
     _running = True
     target_frame_time = 1/fps_limit
+    retval = None
     while _running: #Main loop
         start_time = perf_counter() #Get start time
-        function(*args, **kwargs) #Call function
+        retval = function(*args, **kwargs) #Call function and save returned value
         time_taken = perf_counter() - start_time #Find time elapsed
-        _fps = max(time_taken, fps_limit) #Calculate FPS based on how long this frame took
+        _fps = min(1/time_taken, fps_limit) #Calculate FPS based on how long this frame took
         if(time_taken < target_frame_time): #If no sleep is needed, just continue
             sleep(target_frame_time - time_taken) #Sleep for remaining time
+    return retval #Return last returned value
 
 def endloop():
+    """After the frame that this is called during has completed, exit loop"""
     global _running
     _running = False
 
 def getfps():
+    """Return the semi-instantaneous FPS as defined by 1 divided by the time the last frame took"""
     global _fps
     return _fps
